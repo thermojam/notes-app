2b552fa6412081fed07ee6f63a8679397de496a9
nikita <nmensky@gmail.com>
2025-09-15 22:25:30 +0300

    post request processing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import chalk from "chalk";
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
+import { addNote } from './notes-controller.js'
+
 
 // Запускает сервер на указанном порту
 const port = 3030;
@@ -14,26 +16,28 @@ const basePath = path.join(__dirname, "pages");
 
 // Создаёт HTTP-сервер с обработчиком запросов
 const server = createServer(async (req, res) => {
-    try {
-        if (
-            req.method === "GET" &&
-            (req.url === "/" || req.url === "/index.html")
-        ) {
-            const content = await fs.readFile(
-                path.join(basePath, "index.html"),
-                "utf8"
-            );
-            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-            return res.end(content);
-        }
+    if (req.method === "GET") {
+        const content = await fs.readFile(path.join(basePath, "index.html"));
+        res.writeHead(200, { "content-type": "text/html",})
+        res.end(content);
+        } else if (req.method === "POST") {
+        const body = []
+        res.writeHead(200, {
+            'content-type': 'text/plain; charset=utf-8'
+        })
+        
+        // Событие data позволяет обрабатывать данные по частям, не дожидаясь полной загрузки.
+        req.on('data',  data => {
+            body.push(Buffer.from(data))
+            // Buffer — это класс в Node.js, который предназначен для работы с бинарными данными.
+            // Он позволяет эффективно хранить и манипулировать двоичными данными, например, при работе с файлами, сетевыми потоками
+        })
 
-        // другой путь или метод
-        res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
-        return res.end("Not Found");
-    } catch (err) {
-        console.error(chalk.red("Ошибка при обработке запроса:"), err);
-        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
-        return res.end("Internal Server Error");
+        req.on('end', () => {
+           const title = body.toString().split('=')[1].replaceAll('+', ' ')
+           addNote(title)
+           res.end(`Title = ${title}`);
+        })
     }
 });
 
