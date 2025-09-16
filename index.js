d780630d175b098c9d92a47a95295e814ba7a8a6
nikita <nmensky@gmail.com>
2025-09-16 14:36:51 +0300

    creating a template ejs

--- index.js
+++ index.js
@@ -2,30 +2,36 @@
 // Меньше кода для создания серверов и API, готовые решения для типичных задач
 // Встроенная поддержка шаблонизаторов для генерации HTML
 import express from 'express'
-import { fileURLToPath } from "url";
 import chalk from "chalk";
-import path from "path";
-import { addNote } from './notes-controller.js';
+import { addNote, getNotes } from './notes-controller.js';
 
 const port = 3030;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const basePath = path.join(__dirname, "pages");
+const app = express();
 
-const app = express()
+// Позволяет задавать настройки, которые влияют на поведение Express-приложения
+app.set('view engine', 'ejs');
+app.set('views', 'pages');
+
 // use используется для подключения middleware-функций к приложению Express
 app.use(express.urlencoded({
     // используется библиотека qs, которая позволяет парсить более сложные вложенные объекты и структуры
     extended: true
 }))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(basePath, "index.html"))
+app.get('/', async (req, res) => {
+    // Метод render используется для генерации HTML-кода на основе шаблона EJS и переданных данных
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes()
+    })
 })
 
 app.post('/', async (req, res) => {
     await addNote(req.body.title)
-    res.sendFile(path.join(basePath, "index.html"))
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes()
+    })
 })
 
 // Номер порта для прослушивания (параметр listen)
